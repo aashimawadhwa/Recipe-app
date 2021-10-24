d3fd2227f9cf91dd213a518da861edd680812e97
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-10-25 03:19:07 +0530

    Enhanced the frontend by adding header

---
+++ client/src/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './header.css' ;
+ const header  = () => {
+    return(
+    <header>
+         
+
+  <div className="overlay">
+    <h1>Your E<i className="material-icons prefix">soup_kitchen</i>kitchen</h1>
+    <br />
+  </div>
+</header>
+
+
+
+    );
+}
+export default header ;
